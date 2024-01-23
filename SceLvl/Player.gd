9c41d9ccd6fa955106e1f6b34bfcbea6dcf5872c
Wolve-Wolvaisons <96143217+Logaaaan@users.noreply.github.com>
2024-01-23 08:11:16 -0800

    FloorMovement

--- SceLvl/Player.gd
+++ SceLvl/Player.gd
@@ -1,9 +1,10 @@
 extends Node2D
 class_name Player
 
-const ACC = 4
-const TSP = 250
-const DCC = 5
+const ACC = 3
+const TSP = 360
+const DCC = 3
+const SKD = 27
 const MSP = 400
 const GRV = 16
 const JSP = -400
@@ -24,7 +25,7 @@ var ang = 0
 var hrd = 7.5
 var vrd = 13
 var isGrounded = false
-var aState = 0 #0: Stand 1: Move 2: Roll 3: Duck
+var aState = 0 #0: Stand 1: Move 2: Roll 3: Duck 4: skid
 var dir = 1
 
 # Called when the node enters the scene tree for the first time.
@@ -34,7 +35,7 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	airMove()
+	floorMove()
 	sprite.flip_h = dir == -1
 	anim()
 
@@ -52,22 +53,51 @@ func airMove():
 	var InputDir = Input.get_axis("Left","Right")
 	if InputDir:
 		if hsp * sign(InputDir) < TSP:
-			hsp = move_toward(hsp,TSP*InputDir,ACC*.75)
+			hsp = move_toward(hsp,TSP*InputDir,ACC)
 	else:
-		hsp = move_toward(hsp,0,DCC*.75)
+		hsp = move_toward(hsp,0,DCC)
 	global_position += Vector2(hsp,asp)/60
 
 func floorMove():
-	return
+	aState = 0
+	var InputDir = Input.get_axis("Left","Right")
+	if abs(gsp)>ACC*2:
+		aState = 1
+	if InputDir:
+		dir = sign(InputDir)
+		if gsp * sign(InputDir) < TSP:
+			gsp = move_toward(gsp,TSP*InputDir,ACC)
+		if sign(gsp*InputDir) < 0:
+			aState = 4
+			gsp = move_toward(gsp,TSP*InputDir,SKD)
+	else:
+		gsp = move_toward(gsp,0,DCC)
+		
+	global_position += Vector2.from_angle(ang+0)*gsp/60
+	ang = getFloorAngle()
 
 func getFloorAngle():
 	return 0
 
 func anim():
 	match aState:
+		0:
+			animPl.play("idle")
+			sprite.rotation = 0
+			
 		2:
 			sprite.rotate(PI/5*dir)
 			animPl.play("roll")
+		1:
+			sprite.rotation = floor((ang/(PI/4)))*(PI/4)
+			if gsp > TSP - 20:
+				animPl.play("coast")
+			else:
+				animPl.play("run")
+			animPl.speed_scale = sqrt(abs(gsp/TSP))
+		4:
+			sprite.rotation = floor((ang/(PI/4)))*(PI/4)
+			animPl.play("skid")
 		_:
 			return
 			animPl.play("run")
